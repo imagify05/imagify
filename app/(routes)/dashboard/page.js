ad64e0cf34bd8278556a944ac700c5392a4ef1a8
Nilav <nilavtalukdar06@gmail.com>
2025-05-24 14:49:37 +0530

    added hero section and dashboard route

---
+++ app/(routes)/dashboard/page.js
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <section>
+      <h1>Dashboard Page</h1>
+    </section>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import LoginButton from "@/components/login-button";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -20,6 +21,29 @@ export default function Home() {
           <LoginButton />
         </div>
       </header>
+      <div className="my-10 max-w-7xl mx-auto p-4">
+        <div className="px-5 py-2 bg-white max-w-[273px] mx-auto rounded-[50px] flex justify-center items-center border-[0.3px] border-gray-300 animate-bounce">
+          <p className="text-[15px] text-[#696969]">
+            Best text to image generator 💫
+          </p>
+        </div>
+        <p className="my-7 text-[80px] text-center leading-[80px]">
+          Turn text to <br /> <span className="text-[#007aff]">image</span>, in
+          seconds
+        </p>
+        <p className="my-5 text-center text-[18px] text-[#545454] max-w-[576px] mx-auto">
+          Unleash your creativity with AI. Turn your imagination into visual art
+          in seconds - just type, and watch the magic happen.
+        </p>
+        <Link
+          href="/dashboard"
+          className="w-full flex justify-center items-center my-5"
+        >
+          <Button className="w-[239px] h-[55px] rounded-[50px]">
+            Generate Images ✨
+          </Button>
+        </Link>
+      </div>
     </section>
   );
 }
