dfc5c5ba67fa251d77b917f4fe8a0fbedc32dfce
Nilav <nilavtalukdar06@gmail.com>
2025-05-25 23:13:27 +0530

    add api routes for updating user credits

---
+++ app/api/update-credits/route.js
@@ -0,0 +1,70 @@
+import connectToDb from "@/db/mongodb";
+import userModel from "@/models/user.model";
+import { NextResponse } from "next/server";
+
+export async function PATCH(request) {
+  const { user_id, plan } = await request.json();
+  if (!user_id || !plan) {
+    return NextResponse.json(
+      {
+        message: "incomplete fields",
+      },
+      { status: 400 }
+    );
+  }
+  try {
+    await connectToDb();
+    const user = await userModel.findOne({ user_id: user_id });
+    if (!user) {
+      return NextResponse.json(
+        {
+          message: "user not found",
+        },
+        { status: 403 }
+      );
+    }
+    if (plan === "Basic") {
+      await userModel.updateOne({ user_id: user_id }, { $set: { token: 100 } });
+      return NextResponse.json(
+        {
+          message: "updated credits",
+        },
+        { status: 200 }
+      );
+    } else if (plan === "Advanced") {
+      await userModel.updateOne({ user_id: user_id }, { $set: { token: 500 } });
+      return NextResponse.json(
+        {
+          message: "updated credits",
+        },
+        { status: 200 }
+      );
+    } else if (plan === "Enterprise") {
+      await userModel.updateOne(
+        { user_id: user_id },
+        { $set: { token: 5000 } }
+      );
+      return NextResponse.json(
+        {
+          message: "updated credits",
+        },
+        { status: 200 }
+      );
+    } else {
+      return NextResponse.json(
+        {
+          message: "invalid plan",
+        },
+        { status: 403 }
+      );
+    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        message: `internal server error, error: ${error}`,
+      },
+      { status: 500 }
+    );
+  }
+}
