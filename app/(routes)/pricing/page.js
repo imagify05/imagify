3d88b9737d4a9eea0f3f191af89083505e4e1146
Nilav <nilavtalukdar06@gmail.com>
2025-05-25 22:14:00 +0530

    added pricing page

--- app/(routes)/pricing/page.js
+++ app/(routes)/pricing/page.js
@@ -1,7 +1,11 @@
 export default function PricingPage() {
   return (
-    <section>
-      <h1></h1>
+    <section className="w-full h-full">
+      <div className="max-w-7xl mx-auto p-4 my-10 sm:my-20">
+        <div className="px-5 py-2 bg-white max-w-[164px] mx-auto rounded-[50px] flex justify-center items-center border-[0.3px] border-gray-300 animate-bounce">
+          <p className="text-[15px] text-[#696969] uppercase">Our Plans</p>
+        </div>
+      </div>
     </section>
   );
 }
