69184ff68662a4e52dd8fe60f7d1df58bee8c1cf
Nilav <nilavtalukdar06@gmail.com>
2025-05-26 11:18:57 +0530

    added bot protection

--- middleware.js
+++ middleware.js
@@ -1,4 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import arcjet, { createMiddleware, detectBot } from "@arcjet/next";
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:MONITOR"],
+    }),
+  ],
+});
 
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
@@ -7,11 +18,16 @@ const isPublicRoute = createRouteMatcher([
   "/api(.*)",
 ]);
 
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
-  }
-});
+const combinedMiddleware = createMiddleware(
+  aj,
+  clerkMiddleware(async (auth, req) => {
+    if (!isPublicRoute(req)) {
+      await auth.protect();
+    }
+  })
+);
+
+export default combinedMiddleware;
 
 export const config = {
   matcher: [
@@ -19,5 +35,6 @@ export const config = {
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
+    "/((?!_next/static|_next/image|favicon.ico).*)",
   ],
 };
