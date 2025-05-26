db151c4a76f1460ee3a5dfb3f14ef56b968fc558
Nilav <nilavtalukdar06@gmail.com>
2025-05-26 10:17:49 +0530

    added update credits function to update credits

--- app/(routes)/_components/subscription.jsx
+++ app/(routes)/_components/subscription.jsx
@@ -3,10 +3,34 @@
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import toast from "react-hot-toast";
 
 export default function Subscription({ plan }) {
   const { user } = useUser();
   const router = useRouter();
+
+  const updateCredits = async () => {
+    try {
+      const response = await fetch("/api/update-credits", {
+        method: "PATCH",
+        body: JSON.stringify({
+          user_id: user?.id,
+          plan: plan,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        throw new Error(response.status, response.statusText);
+      }
+      const data = await response.json();
+      console.log(data.message);
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to update credits");
+    }
+  };
 
   const handlePayment = () => {
     console.log("paid");
